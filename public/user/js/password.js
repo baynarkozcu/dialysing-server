b199889bb8cfab89b5a9a6ccf48196544d5ea4bf
bzceval <busraaceval@gmail.com>
2022-07-17 00:01:16 +0300

    add password secret

--- public/user/js/password.js
+++ public/user/js/password.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     // değiştir göz ikonuna tıklandığında
     $("#degistir").click(function () {
         // eğer type niteliği password ise
-        if ($(".sifre").attr("type") == "password") {
+        if ($(".password").attr("type") == "password") {
             // type niteliğini text olarak değiştir
-            $(".sifre").attr("type", "text");
+            $(".password").attr("type", "text");
         }
         // password değil text ise
         else {
             // type niteliğini password olarak değiştir
-            $(".sifre").attr("type", "password");
+            $(".password").attr("type", "password");
         }
     });
 });

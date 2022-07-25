d74ea68ce423a4182a8e27c7105e484ceff5723a
bzceval <busraaceval@gmail.com>
2022-07-26 01:15:33 +0300

    add register password eye

--- public/user/js/password.js
+++ public/user/js/password.js
@@ -1,6 +1,24 @@
+//LOGIN PASSWORD EYE
 $(document).ready(function () {
     // değiştir göz ikonuna tıklandığında
     $("#degistir").click(function () {
+        // eğer type niteliği password ise
+        if ($(".password").attr("type") == "password") {
+            // type niteliğini text olarak değiştir
+            $(".password").attr("type", "text");
+        }
+        // password değil text ise
+        else {
+            // type niteliğini password olarak değiştir
+            $(".password").attr("type", "password");
+        }
+    });
+});
+
+//REGISTER PASSWORD EYE
+$(document).ready(function () {
+    // değiştir göz ikonuna tıklandığında
+    $("#register-degistir").click(function () {
         // eğer type niteliği password ise
         if ($(".password").attr("type") == "password") {
             // type niteliğini text olarak değiştir
